6e017613772eeadd856a33e93b5ff5f4c3e7b9d9
Dave Plummer <dave@daveandnikki.com>
2022-04-02 16:27:41 -0700

    Wrover support

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,119 @@
-#include <M5Core2.h>
-#include "../images/MachineBackground.c"
+#include <Arduino.h>
+#include <TinyGPSPlus.h>
 
-void setup() {
-  M5.begin(); 
-  M5.Lcd.fillScreen(WHITE); 
-  delay(500); 
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(10, 10); //Move the cursor position to (x,y).  移动光标位置到 (x,y)处
-  M5.Lcd.setTextColor(WHITE); //Set the font color to white.  设置字体颜色为白色
-  M5.Lcd.setTextSize(3);  //Set the font size.  设置字体大小
-  M5.Lcd.printf("Display Test!");  //Serial output format string.  输出格式化字符串
-}                                                         // Construct a triangle with (x1,y1) (x2,y2) (x3,y3) as its vertices
+#define USER_SETUP_LOADED 1
+#define ST7789_DRIVER 1
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 320
+#define TFT_MISO 25
+#define TFT_MOSI 23
+#define TFT_SCLK 19
+#define TFT_CS 22 // Chip select control pin
+#define TFT_DC 21 // Data Command control pin
+#define TFT_RST 18 // Invoke custom library
+#define TFT_BL 5
+#define LOAD_GLCD 1
+
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft;
+
+/* 
+   This sample sketch should be the first you try out when you are testing a TinyGPSPlus
+   (TinyGPSPlus) installation.  In normal use, you feed TinyGPSPlus objects characters from
+   a serial NMEA GPS device, but this example uses static strings for simplicity.
+*/
+
+// A sample NMEA stream.
+const char *gpsStream =
+  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
+
+HardwareSerial SerialGPS(2);
+
+void setup()
+{
+  SerialGPS.begin(9600, SERIAL_8N1, 35, 34);      // 33, 32 for Core2
+  Serial.begin(115200);
+
+  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println();
+
+  pinMode(TFT_BL, OUTPUT);
+
+   tft.init();
+   tft.fillScreen(TFT_BLUE);
+}
 
 void loop()
 {
-  /*
-  unsigned long d = millis();
-  int w3 = M5.Lcd.width() / 3;
-  M5.Lcd.fillRect(0, 0, w3, M5.Lcd.height(), random(0xfffe));
-  M5.Lcd.fillRect(w3, 0, w3, M5.Lcd.height(), random(0xfffe));
-  M5.Lcd.fillRect(w3*2, 0, w3, M5.Lcd.height(), random(0xfffe));
-  M5.update();  //Read the press state of the key.  读取按键 A, B, C 的状态
-  unsigned long delta = millis() - d;
-  M5.Lcd.setCursor(10, 10); //Move the cursor position to (x,y).  移动光标位置到 (x,y)处
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.printf("FPS: %d", 1000 / delta);
-  */
-  unsigned long d = millis();
-//  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.drawBitmap(0, 0, 320, 240, image_data_MachineBackground);
-  unsigned long delta = millis() - d;
-  M5.Lcd.setCursor(10, 10); //Move the cursor position to (x,y).  移动光标位置到 (x,y)处
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.printf("FPS: %d", 1000 / delta);
+  while (SerialGPS.available())
+  {
+    char x = SerialGPS.read();
+    Serial.write(x);
+    if (gps.encode(x))
+    {
+      Serial.printf(">> ");
+      displayInfo();
+    }
+  }
 }
